a6b324801b3956edc8f7760d433e6716330f6c01
shioyang <shioyang4321@gmail.com>
2020-12-05 05:06:26 +0900

    feat: story 2 - step 3: add "+" button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CountPanelComponent } from './count-panel/count-panel.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, CountPanelComponent],
+  declarations: [AppComponent, CountPanelComponent, ButtonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
